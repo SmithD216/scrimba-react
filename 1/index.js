05972e501c58dbf13a7fb2b5b954e0c412201d43
spindleshanks <smithd216@protonmail.com>
2023-02-02 19:58:33 +0000

    Initial commit

---
+++ 1/index.js
@@ -0,0 +1,11 @@
+function MainContent(){
+    return (
+        <h1>I'm learning react!</h1>
+        )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,
+     document.getElementById("root"));
