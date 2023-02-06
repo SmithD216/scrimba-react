32d305e973ea25d783ac79a4629aa914bcb9d180
spindleshanks <smithd216@protonmail.com>
2023-02-06 15:15:07 +0000

    Structure is finished and basic styling completed

--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import '../styles/card.css'
 
 export default function Card(props){
     return(
-        <div>
-             <h2>{props.item.title}</h2>
-            location: {props.item.location}
-            <a href={`"${props.item.googleMapsUrl}"`}>{props.item.googleMapsUrl}</a>
-            <p>{props.item.startDate}</p>
-            <p>{props.item.endDate}</p>
-            <p> {props.item.description}</p>
+        <div className='card'>
+            <div className='card--left'>
+                <img className='card--image' src={`${props.item.imageUrl}`} />
+            </div>
+            <div className='card--right' >
+                <p className='card--location'>{props.item.location}</p>
+                <a className='card--url' href={`${props.item.googleMapsUrl}`}>View on Google maps</a>
+                <h2 className='card--title'>{props.item.title}</h2>
+                <p className='card--startdate' >{props.item.startDate} - </p>
+                <p className='card--enddate' >{props.item.endDate}</p>
+                <p className='card--description'> {props.item.description}</p>
+            </div>
+            
         </div>
     )
 }
