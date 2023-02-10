5bb2c6b4f7ce29078a038429c75cceee351164a4
spindleshanks <smithd216@protonmail.com>
2023-02-10 14:35:41 +0000

    Added meme data and loggin images to console

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -4,8 +4,6 @@ import Heading from './components/Heading'
 import Meme from './components/Meme'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Heading />
--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../styles/meme.css';
+import memesData from '../memesData.js'
 
 export default function Meme(){
+    function getMeme(){
+        const memes = memesData.data.memes
+        console.log(memes[Math.floor(Math.random() * 100) + 1].url);
+    }
+
     return(
-        <form action="" className="meme--form">
+        <div className="meme--form">
             <input type="text" className="meme--top"/>
             <input type="text" className="meme--bottom"/>
-            <button className="meme--button">Get a new meme image &#128444;&#65039;</button>
-        </form>
+            <button className="meme--button" onClick={getMeme}>Get a new meme image &#128444;&#65039;</button>
+        </div>
     )
 }
