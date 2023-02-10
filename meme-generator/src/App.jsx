1a741cebecffb3a2475f1317582f64320b6e13bf
spindleshanks <smithd216@protonmail.com>
2023-02-10 18:13:39 +0000

    Added image generation

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Heading from './components/Heading'
 import Meme from './components/Meme'
--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/meme.css';
 import memesData from '../memesData.js'
 
 export default function Meme(){
+    const [maymay, setMayMay] = useState("")
+
     function getMeme(){
         const memes = memesData.data.memes
-        console.log(memes[Math.floor(Math.random() * 100) + 1].url);
+        const result = memes[Math.floor(Math.random() * 100) + 1].url;
+        setMayMay(result)
     }
-
+    
     return(
+        <>
         <div className="meme--form">
             <input type="text" className="meme--top"/>
             <input type="text" className="meme--bottom"/>
             <button className="meme--button" onClick={getMeme}>Get a new meme image &#128444;&#65039;</button>
         </div>
+        <img src={maymay}/>
+        </>
     )
 }
