ccf47b6155c7aa92cc4f55b29ed2c81076773271
spindleshanks <smithd216@protonmail.com>
2023-02-13 18:42:29 +0000

    Refactoring meme component

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -3,12 +3,19 @@ import '../styles/meme.css';
 import memesData from '../memesData.js'
 
 export default function Meme(){
-    const [maymay, setMayMay] = useState("")
+    const [maymay, setMayMay] = useState("");
+    const [meme, setMeme] = useState({topText: "", bottomText: "", randomImage: ""});
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getMeme(){
-        const memes = memesData.data.memes
+        const memes = allMemeImages.data.memes
         const result = memes[Math.floor(Math.random() * 100) + 1].url;
-        setMayMay(result)
+        setMeme(prevMeme=>{
+            return{
+                ...prevMeme,
+                randomImage: result
+            }
+        })
     }
     
     return(
@@ -18,7 +25,7 @@ export default function Meme(){
             <input type="text" className="meme--bottom"/>
             <button className="meme--button" onClick={getMeme}>Get a new meme image &#128444;&#65039;</button>
         </div>
-        <img className='meme--image' src={maymay}/>
+        <img className='meme--image' src={meme.randomImage}/>
         </>
     )
 }
